e89e66dda7999abddb545a8a628c3f745864a51a
blavikensbutcher <luckertheory@gmail.com>
2024-04-03 20:52:10 +0300

    change comments

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -10,19 +10,22 @@ export async function listContacts() {
 }
 
 export async function getContactById(contactId) {
-  //Get data from db
+  //Get one contact from db
   return Contact.findById(contactId);
 }
 
 export async function removeContact(contactId) {
+  //Get one contact from db and delete
   return Contact.findByIdAndDelete(contactId);
 }
 
 export async function addContact(name, email, phone, favorite) {
+  //Post new contact to db
   return Contact.create({ name, email, phone, favorite });
 }
 
 export async function changeContact(contactID, name, email, phone, favorite) {
+  //Find contact by id and update field/fields
   return Contact.findByIdAndUpdate(
     contactID,
     { contactID, name, email, phone, favorite },
@@ -31,5 +34,6 @@ export async function changeContact(contactID, name, email, phone, favorite) {
 }
 
 export async function updateStatusContact(contactID, favorite) {
+  //Find contact by id and change field favorite
   return Contact.findByIdAndUpdate(contactID, { favorite }, { new: true });
 }
