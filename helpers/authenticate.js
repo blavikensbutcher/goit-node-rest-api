8be87f2c023ded1bf8ea69e61147ef9cd457407f
blavikensbutcher <luckertheory@gmail.com>
2024-04-13 12:04:19 +0300

    add jdoc

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -4,13 +4,13 @@ import { User } from "../schemas/usersSchema.js";
 
 export const authenticate = async (req, res, next) => {
   /////GET Bearer and Token from Headers
-  const { authorization = "" } = req.headers;
+  const { authorization = "" } = req.headers; /** @returns: undefined if didn't find and split throws error. Set empty string for default */
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
     next(httpError(401));
   }
-  
+
   try {
     /////// Check jwt is valid if not throw error
     const { id } = isJwtValid(token);
